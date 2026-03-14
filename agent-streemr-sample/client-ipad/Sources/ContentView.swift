72f603c99e16496ac1b194caa275b3d5349c22af
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2026-03-13 21:34:33 -0700

    Initialized sample ipad app for better demoing

---
+++ agent-streemr-sample/client-ipad/Sources/ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("AgentStreemr")
+            .font(.largeTitle)
+            .foregroundStyle(.secondary)
+    }
+}
+
+#Preview {
+    ContentView()
+}
